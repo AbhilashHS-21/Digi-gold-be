51c134910bb4d4a25893411d837b76c21feed12d
AbhilashHS-21 <abhilash2k21@gmail.com>
2025-11-08 22:43:30 +0530

    Fix route path for retrieving all users' SIPs to ensure proper access control

--- src/routes/sipRoutes.js
+++ src/routes/sipRoutes.js
@@ -5,7 +5,7 @@ import { ensureAdmin } from '../middlewares/adminMiddleware.js'
 
 const router = express.Router();
 router.post("/fixed/create", authMiddleware, ensureAdmin, createFixedSip);
-router.get("/", authMiddleware, ensureAdmin, getAllUsersSips);
+router.get("/all", authMiddleware, ensureAdmin, getAllUsersSips);
 
 router.post("/fixed/opt", authMiddleware, optFixedSip);
 router.post("/flexible/create", authMiddleware, createFlexibleSip);
